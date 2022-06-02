4e84c8067d5faf27d6bc685308f6a42f033eacfe
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-06-02 21:45:26 +0300

    update

--- 7 - Mac Changer/mac_changer.py
+++ 7 - Mac Changer/mac_changer.py
@@ -1,16 +1,21 @@
+from ast import arguments
 import imp
 import subprocess
 import optparse as opt
 
 parse_object = opt.OptionParser()
-parse_object.add_option("--i","interface",dest="interface",help="interface to change")
+parse_object.add_option("-i","interface",dest="interface",help="interface to change")
+parse_object.add_option("-m","--mac",dest="mac_address",help="new mac address")
 
+(user_inputs,arguments) = parse_object.parse_args()
+print(user_inputs.interface)
+print(user_inputs.mac_address)
 
 interface = "eth0"
-mac_add = "00:11:22:33:33:44"
+mac_address = "00:11:22:33:33:44"
 
 print("My Mac Changer Started!")
 
 subprocess.call(["ifconfig",interface,"down"])
-subprocess.call(["ifconfig",interface,"hw",mac_add])
+subprocess.call(["ifconfig",interface,"hw",mac_address])
 subprocess.call(["ifconfig",interface,"up"])

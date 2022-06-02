59f6d67e25fb71c4579af445d36e4fc414087382
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-06-02 21:30:31 +0300

    update

--- 7 - Mac Changer/mac_changer.py
+++ 7 - Mac Changer/mac_changer.py
@@ -1,7 +1,16 @@
+import imp
 import subprocess
+import optparse as opt
+
+parse_object = opt.OptionParser()
+parse_object.add_option("--i","interface",dest="interface",help="interface to change")
+
+
+interface = "eth0"
+mac_add = "00:11:22:33:33:44"
 
 print("My Mac Changer Started!")
 
-subprocess.call(["ifconfig","eth0","down"])
-subprocess.call(["ifconfig","eth0","hw","00:11:22:33:33:44"])
-subprocess.call(["ifconfig","eth0","up"])
+subprocess.call(["ifconfig",interface,"down"])
+subprocess.call(["ifconfig",interface,"hw",mac_add])
+subprocess.call(["ifconfig",interface,"up"])

8dffd76709e13dda5491d289f6f3165c46b425c7
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-06-03 23:58:03 +0300

    added new control

--- 7 - Mac Changer/mac_changer.py
+++ 7 - Mac Changer/mac_changer.py
@@ -1,6 +1,7 @@
 from re import sub
 import subprocess
 import optparse as opt
+import re
 
 def get_user_input():
     parse_object = opt.OptionParser()
@@ -15,11 +16,20 @@ def change_mac_address(user_interface,user_mac_address):
 
 def control_new_mac(interface):
     ifconfig = subprocess.check_output(["ifconfig",interface])
-    print(ifconfig)
+    new_mac_address = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",ifconfig)
+    if new_mac_address:
+        return new_mac_address.group(0)
+    else:
+        return None
     
     
 print("My Mac Changer Started!")
 
 (user_input,arguments) = get_user_input()
 change_mac_address(user_input.interface,user_input.mac_address)
-control_new_mac(user_input.interface)
+finalized_mac = control_new_mac(user_input.interface)
+
+if finalized_mac == user_input.mac_address:
+    print("Success!")
+else:
+    print("Error!")

